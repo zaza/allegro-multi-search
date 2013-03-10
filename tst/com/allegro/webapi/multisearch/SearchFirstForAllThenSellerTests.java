6593a20bf243e54b1b323b1300c4e1c306eec0cb
Tomasz Zarna <tomasz.zarna@tasktop.com>
2013-03-10 01:39:40 +0100

    remove unused import

--- tst/com/allegro/webapi/multisearch/SearchFirstForAllThenSellerTests.java
+++ tst/com/allegro/webapi/multisearch/SearchFirstForAllThenSellerTests.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
